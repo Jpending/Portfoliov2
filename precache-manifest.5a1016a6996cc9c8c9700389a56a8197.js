855db7acd4e4b25201653bda6f7de50363bbd2aa
John <jpending@gmail.com>
2020-09-29 19:01:46 -0400

    Updates

---
+++ precache-manifest.5a1016a6996cc9c8c9700389a56a8197.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2a55798e016e99e098e6f9c6b49f8af",
+    "url": "/Portfoliov2/s/index.html"
+  },
+  {
+    "revision": "3fb29969b0389fd1ccf2",
+    "url": "/Portfoliov2/s/static/css/main.11d52aba.chunk.css"
+  },
+  {
+    "revision": "a5b2766780ed9c47a05e",
+    "url": "/Portfoliov2/s/static/js/2.4048c3c6.chunk.js"
+  },
+  {
+    "revision": "349a1a8e960b3e799f5df22fa2e8de3a",
+    "url": "/Portfoliov2/s/static/js/2.4048c3c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb29969b0389fd1ccf2",
+    "url": "/Portfoliov2/s/static/js/main.6d31cd50.chunk.js"
+  },
+  {
+    "revision": "545bdc813dd0a27bc52b",
+    "url": "/Portfoliov2/s/static/js/runtime-main.cdf9421c.js"
+  },
+  {
+    "revision": "706582094392b8b70bde7e0f4b369921",
+    "url": "/Portfoliov2/s/static/media/facebook.70658209.png"
+  },
+  {
+    "revision": "d86243da1a04184b7e642a110fa92fac",
+    "url": "/Portfoliov2/s/static/media/github-logo.d86243da.png"
+  },
+  {
+    "revision": "67821c705b2e7332acb6902608093c6c",
+    "url": "/Portfoliov2/s/static/media/ironshadewow_ss.67821c70.jpeg"
+  },
+  {
+    "revision": "2a83015ccb9d191ebb8b91edd0277eee",
+    "url": "/Portfoliov2/s/static/media/jurps_ss.2a83015c.jpg"
+  },
+  {
+    "revision": "102bb412c7a5ed62c8ce66f333d5e58a",
+    "url": "/Portfoliov2/s/static/media/linkedin-logo.102bb412.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Portfoliov2/s/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "7468d5912306b5f4d7e9d0de6e0080f1",
+    "url": "/Portfoliov2/s/static/media/luckypick_ss.7468d591.jpg"
+  },
+  {
+    "revision": "0c58dcd7035a88a5a1830e1136806e79",
+    "url": "/Portfoliov2/s/static/media/ninja.0c58dcd7.jpg"
+  },
+  {
+    "revision": "5025c245d6788433b02fe4b215e56b15",
+    "url": "/Portfoliov2/s/static/media/ninja2.5025c245.jpg"
+  },
+  {
+    "revision": "1cf524608de16d62ec9a8f4c204e9717",
+    "url": "/Portfoliov2/s/static/media/ocean.1cf52460.jpg"
+  },
+  {
+    "revision": "ad193ac0de5964d4fe533f375d5e49f6",
+    "url": "/Portfoliov2/s/static/media/smoker.ad193ac0.jpg"
+  },
+  {
+    "revision": "db292a18cc23331d2724503b4ead9cd5",
+    "url": "/Portfoliov2/s/static/media/twitter.db292a18.png"
+  },
+  {
+    "revision": "992dbaf8a1f5fa5f7535b34322b9e6c4",
+    "url": "/Portfoliov2/s/static/media/us3.992dbaf8.jpg"
+  },
+  {
+    "revision": "8e5c11c766ed81166e3658c6376e5eec",
+    "url": "/Portfoliov2/s/static/media/wow.8e5c11c7.jpg"
+  }
+]);
